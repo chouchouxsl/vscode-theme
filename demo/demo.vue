db9039a1d96e90f143604db9ee66778ed23a4010
R_h_zero <renhongze@able-elec.com>
2022-05-05 08:49:43 +0800

    test: demo

---
+++ demo/demo.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="containar"></div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: '',
+    components: {},
+    setup() {
+        return {
+
+        }
+    },
+})
+</script>
+
+<style scoped lang='scss'>
+.containar {
+  width: 100px;
+  height: 100px;
+}
+</style>
